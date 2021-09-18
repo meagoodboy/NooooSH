d271f2a69c1a9fccf103749b109869dd90a71f0e
Aswin Jose <aswin.jose@research.iiit.ac.in>
2021-09-18 15:02:23 +0530

    bg string parsing left

--- shell.c
+++ shell.c
@@ -28,7 +28,13 @@
 #define INP_SIZE 4096
 
 int total_children_bg = 0;
+int history_count = 0;
+char* history_list[20] = {NULL};
 int children_pid[1000];
+struct child_process{
+    char childname[1000];
+    int child_id;
+}cp[100];
 extern int errno;
 char USER[100];
 char HOST[100];
@@ -38,6 +44,100 @@ char LWD[1000];
 
 
 int runcommand(char* words[] , int word_count);
+
+int initHistory(){
+    //malloc hisory
+    for(int i = 0; i< 20 ; i++){
+        history_list[i] = (char *)malloc(INP_SIZE);
+    }
+    char * temp_history = (char *)malloc(INP_SIZE);
+    char * history_pointer;
+    FILE *history_loc;
+    history_loc = fopen("/var/tmp/shell_history.txt", "r");
+    if(history_loc == NULL){
+        history_count = 0;
+    }
+    else{
+        history_count = 0;
+        fread(temp_history, 1, 20 * INP_SIZE, history_loc);
+        history_pointer = strtok(temp_history,"\n");
+        while(history_pointer){
+            strcpy(history_list[history_count], history_pointer);
+            history_count ++;
+            history_pointer = strtok(NULL,"\n");
+        }
+        fclose(history_loc);
+    }
+    free(temp_history);
+    printf("\nHistory iniitalised sucessfullly");
+    return 0;
+}
+
+int closeHistory(){
+    
+    char* all_history = (char*) malloc (20 * INP_SIZE);
+    for(int i = 0 ;i < history_count; i++){
+        strcat(all_history, history_list[i]);
+        strcat(all_history, "\n");
+    }
+    FILE *history_loc;
+    history_loc = fopen("/var/tmp/shell_history.txt", "w");
+    fwrite(all_history, 1, strlen(all_history) , history_loc);
+    fclose(history_loc);
+    free(all_history);
+    for(int i = 0; i< 20 ; i++){
+        free(history_list[i]);
+    }
+    return 0;
+}
+
+int addHistory(char* words[], int word_count){
+    if(history_count < 20){ 
+        strcpy(history_list[history_count],words[0]);
+        for(int i = 1 ; i < word_count ; i++){
+            strcat(history_list[history_count], " ");
+            strcat(history_list[history_count], words[i]);
+
+        }
+        history_count ++;
+    }else{
+        for(int i = 0 ; i < 19 ; i ++){
+            strcpy(history_list[i],history_list[i+1]);
+        }
+        strcpy(history_list[19],words[0]);
+        for(int i = 1 ; i < word_count ; i++){
+            strcat(history_list[19], " ");
+            strcat(history_list[19], words[i]);
+
+        }
+    }
+    return 0;
+}
+
+int getHistory(char* words[], int word_count){
+    if(word_count > 2){
+        printf("\nhistory : invalid no of arguments");
+        return 1;
+    }
+    else if(word_count == 1){
+        for (int i = 0 ; i < history_count ; i++){
+            printf("\n%s", history_list[i]);
+        }
+    }
+    else{
+        if(atoi(words[1]) > history_count){
+            printf("\nhistory : invalid history no");
+            return 1;
+        }
+        for (int i = history_count - atoi(words[1]) ; i < history_count ; i++){
+            printf("\n%s", history_list[i]);
+        }
+    }
+    return 0;
+}
+
+
+
 // function to initialise basic environment variables 
 int initialise(){
     clear;
@@ -46,6 +146,7 @@ int initialise(){
     printf("   WELCOME TO A REGULAR SHELL EXCLUSIVELY FOR OSN PURPOSES");
     printf("\n ************************************************************ \n\n");
     reset_color();
+    initHistory();
     strcpy(USER, getenv("USER"));
     strcpy(HOME, getenv("HOME"));
     getcwd(PWD, sizeof(PWD));
@@ -53,7 +154,6 @@ int initialise(){
     gethostname(HOST, sizeof(HOST));
     return errno;
 }
-
 
 // function to create the PWD path
 char* formatpath(){
@@ -130,7 +230,7 @@ int cd(char* words[] , int word_count){
     printf("\n%s", newdir);
     strcpy(LWD, PWD);
     int x = chdir(newdir);
-    if ( x < 0 ) perror("lol");
+    if ( x < 0 ) perror("cd ");
     }
     return 0;
 }
@@ -176,13 +276,18 @@ int print_l_ls(char* file_name , char* dir_name){
     if( stat(final_dir_name, &f_info) != -1){
         struct group *group_d = getgrgid(f_info.st_gid);
         struct passwd *passwd_d = getpwuid(f_info.st_uid);
-        char *time = (char *)malloc(100); 
-        strftime(time, 50, "%b %d %H:%M", localtime( &f_info.st_mtime));
-        printf("\n%s %ld %s %s %ld %s %s", permissions(final_dir_name), f_info.st_nlink , passwd_d->pw_name, group_d->gr_name , f_info.st_size , time , dir_name);
+        char *time_c = (char *)malloc(100); 
+        char* time_format = "%b %d %R";
+        if(labs(time(NULL) - f_info.st_mtim.tv_sec) > (6*2629743)){
+            time_format = "%b %d %Y";
+        }
+        strftime(time_c, 50, "%b %d %R", localtime( &f_info.st_mtime));
+        printf("\n%s %ld %s %s %ld %s %s", permissions(final_dir_name), f_info.st_nlink , passwd_d->pw_name, group_d->gr_name , f_info.st_size , time_c , dir_name);
     }
     else{
         printf("\n name :  %s", final_dir_name);
     }
+    return 0;
 }
 
 int printls(int a , int l, int relative,  char* f_name){
@@ -304,9 +409,40 @@ int repeat(char* words[] , int word_count){
 
 /// finish these in 1 hour
 int addchild(int pid, char* command){
+    if (total_children_bg > 99){
+        printf("\nadd process : too many children process, no space in pool");
+        return 1;
+    }
+
+    cp[total_children_bg].child_id = pid;
+    strcpy(cp[total_children_bg].childname, command);
+    total_children_bg++;
+
     return 0;
 }
+
 int removechild(int pid){
+    int i;
+    int found = 0;
+    printf("\nremovechilde children : %d", total_children_bg );
+    for(i = 0; i< total_children_bg ; i++){
+        printf("\nstruct : %d", cp[i].child_id);
+        if(pid == cp[i].child_id){
+            found = 1;
+            break;
+        }
+    }
+    if(found == 0){
+        printf("\nremove process : could not locate child process");
+        return 1;
+    }
+    struct child_process temp ;
+    temp.child_id = cp[i].child_id;
+    strcpy(temp.childname ,cp[i].childname);
+    total_children_bg --;
+    for( ; i < total_children_bg  ;i++){
+        cp[i] = cp[i + 1];
+    }
     return 0;
 }
 
@@ -315,15 +451,19 @@ void signalhandler(int signal){
     pid_t pid;
     int pstat;
     printf("Hewewe");
-    while ((pid = waitpid( -1, &pstat, WNOHANG)) >= 0){
+    while ((pid = waitpid( -1, &pstat, WNOHANG)) > 0){
         pstat = WIFEXITED(pstat);
-
+        printf("\nlollbahjbd hid");
         for (int i = 0; i < total_children_bg ; i++){
-            if( children_pid[i] == pid){
-                printf("\n%s exited  %s", pid , (pstat == 0 )? "Normally" : "Abnormally");
+            if( cp[i].child_id == pid){
+                printf("\n%d exited  %s", pid , (pstat == 0 )? "Normally" : "Abnormally");
+                removechild(pid);
+                
             }
         }
     }
+    getPWD();
+    fflush(stdout);
 
 }
 
@@ -349,22 +489,33 @@ int fg(char* words[] , int word_count){
 }
 
 int bg(char* words[] , int word_count){
+    
+    int v_words = word_count;
+    printf("\n bg  : %s", words[0]);
+    
+    words[v_words - 1][strlen(words[v_words - 1]) - 1] = '\0';
+
+
+    if (strcmp(words[v_words - 1],"\0") == 0){
+        printf("\n bg : here");
+        words[v_words - 1] = NULL;
+        v_words --;
+    }
+    printf("\n bg : heretoo");
+    for (int k =0 ; k < v_words; k++)
+    {
+        if(words[k] != NULL)
+        printf("\nbg words: %s", words[k]);
+    }
+    printf("\n bg %s : here3", words[v_words - 1]);
     int pid = fork();
+
     if( pid < 0){
         perror("\nforking ");
         return errno;
     }
-    printf("\n bg  : %s", words[0]);
-    words[word_count - 1][strlen(words[word_count - 1]) - 1] = '\0';
-
-    if (strcmp(words[word_count - 1],"\0") == 0){
-        words[word_count - 1]= NULL;
-    }
-    for (int k =0 ; k < word_count; k++)
-    {
-        printf("\n %s", words[k]);
-    }
     if(pid == 0){
+        printf("\n bg : here4");
         int status = execvp(words[0], words);
         if(status < 0){
             perror("\nexecvp ");
@@ -372,7 +523,13 @@ int bg(char* words[] , int word_count){
         }
 
     }else{
-        children_pid[total_children_bg ++] = pid;
+        printf("\n bg : here5");
+        children_pid[total_children_bg] = pid;
+        int errorchk = addchild(pid, words[0]);
+        if(errorchk != 0){
+            printf("\nerror in child process adding");
+            return 1;
+        }
         struct sigaction sigh;
         sigh.sa_handler = &signalhandler;
         sigemptyset(&sigh.sa_mask);
@@ -380,6 +537,7 @@ int bg(char* words[] , int word_count){
         if (sigaction(SIGCHLD, &sigh, NULL) == -1){
             perror("couldnt signal the process");
         }
+        sleep(1);
     }
     return 0;
 }
@@ -399,68 +557,79 @@ int pinfo(char* words[] , int word_count){
     }
 
     char* proc_path = (char *) malloc (100);
+    char* exec_path = (char *) malloc (100);
     sprintf(proc_path,"/proc/%d/stat", pid);
+    sprintf(exec_path, "/proc/%d/exe", pid);
     FILE* process_info;
     process_info = fopen(proc_path, "r");
     char* full_proc_info = (char *) malloc (INP_SIZE);
     if( process_info == NULL) {
-        perror("\npinfo ");
+        printf("\npinfo : no such process ");
+        return 1;
     }
     else
     {
         fread(full_proc_info, INP_SIZE, 1, process_info);
 
 
-        char* file_stats[100] = {NULL}; int i=0;
+        char* file_stats[100] = {NULL}; 
+        int i=0;
         file_stats[i] = strtok(full_proc_info, " ");
-        while(file_stats[i] != NULL) file_stats[++i] = strtok(NULL, " ");
+        while(file_stats[i]){
+            file_stats[++i] = strtok(NULL, " ");
+        }
+        char exe_path[INP_SIZE ];
+        memset(exe_path, 0, INP_SIZE - 1);
+        int readStat = readlink(exec_path, exe_path, PATH_MAX);
 
-        printf("pid -- %d\n",pid);
-        printf("Process Status -- %s%s\n", file_stats[2], pid == getpid() ? "+":"");
-        printf("memory -- %s\n",file_stats[22]);
-
-        sprintf(proc_path, "/proc/%d/exe", (int)pid);
-        char exe_path[INP_SIZE + 1];
-        memset(exe_path, 0, INP_SIZE);
-        int readStat = readlink(proc_path, exe_path, PATH_MAX);
-        printf("Executable Path -- %s\n", readStat == -1 ? "Doesn't exist" : exe_path);
+        printf("\npid -- %d",pid);
+        printf("\nProcess Status -- %s%s", file_stats[2], pid == getpid() ? "+":"");
+        printf("\nmemory -- %s",file_stats[22]);
+        printf("\nExecutable Path -- %s", readStat == -1 ? "Doesn't exist" : exe_path);
     }
     fclose(process_info);
+    free(proc_path);
+    free(exec_path);
     return 0;
 }
 
 int runcommand(char* words[] , int word_count){
-
+    int rval = 0;
     if(strcmp(words[0], "cd") == 0){
-        cd(words, word_count);
+        rval = cd(words, word_count);
     }
     else if(strcmp(words[0], "exit") == 0){
+        addHistory(words, word_count);
+        closeHistory();
         exit(0);
     }
     else if(strcmp(words[0], "ls") == 0){
-        ls(words, word_count);
+        rval = ls(words, word_count);
     }
     else if(strcmp(words[0], "pwd") == 0){
-        pwd(words, word_count);
+        rval = pwd(words, word_count);
     }
     else if(strcmp(words[0], "echo") == 0){
-        echo(words, word_count);
+        rval = echo(words, word_count);
     }
     else if(strcmp(words[0], "repeat") == 0){
-        repeat(words, word_count);
+        rval = repeat(words, word_count);
     }
     else if(strcmp(words[0], "pinfo") == 0){
-        pinfo(words, word_count);
+        rval = pinfo(words, word_count);
+    }
+    else if(strcmp(words[0], "history") == 0){
+        rval = getHistory(words, word_count);
     }
     else if(strcmp(words[word_count - 1] + strlen(words[word_count - 1]) - 1, "&" ) == 0){
-        bg(words, word_count);
+        rval = bg(words, word_count);
     }
     else{
-        fg(words, word_count);
+        rval = fg(words, word_count);
     }
     
 
-    return 0;
+    return rval;
 }
 
 // function to tokenize and manage the input received
@@ -509,7 +678,9 @@ int manageINPUT(char* input){
             continue;
         }   
 
-        runcommand(words_in_line, words);
+        int x = runcommand(words_in_line, words);
+        if(x == 0);
+            addHistory(words_in_line, words);
 
     }
 
@@ -533,6 +704,6 @@ int main(){
             printf("command : error");
         }
     }
-
+    free(input);
     return 0;
 }
